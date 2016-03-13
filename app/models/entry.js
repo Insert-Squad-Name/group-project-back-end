aaa706df163c8806aabf45109b48d38a39dca3de
kpie89 <kpie89@gmail.com>
2016-03-13 16:10:27 -0400

    took user and page id out of content scope

--- app/models/entry.js
+++ app/models/entry.js
@@ -5,25 +5,25 @@ let mongoose = require('mongoose');
 
 const entrySchema = new mongoose.Schema({
   content: {
-    title: {
-      type: String,
-      required: true
+      title: {
+        type: String,
+        required: true
     },
-    body: {
-      type: String,
-      required: true
-    },
+      body: {
+        type: String,
+        required: true
+    }
+  },
     _userId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
       required: true,
     },
-    _pageId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Page',
-      required: true,
-    }
-  }
+  //   _pageId: {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: 'Page',
+  //     required: true,
+  // }
 }, {
   timestamps: true
 });
