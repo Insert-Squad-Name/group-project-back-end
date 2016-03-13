841d8d8ad9ab6e82835e7c1361fdb9d63bac2100
kpie89 <kpie89@gmail.com>
2016-03-13 13:25:55 -0400

    add basic initial Entry model

---
+++ app/models/entry.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let mongoose = require('mongoose');
+
+
+const entrySchema = new mongoose.Schema({
+  content: {
+    title: {
+      type: String,
+      required: true
+    },
+    body: {
+      type: String,
+      required: true
+    }
+  }
+}, {
+  timestamps: true
+});
+
+const Entry = mongoose.model('Entry', entrySchema);
+
+module.exports = Entry;
